2ee61a9f22179a1efd80fcf156eb0ccfdb39f7fa
shashwat <khabiya.shashwat@gmail.com>
2026-03-04 18:26:11 +0530

    commit this change

--- lib/utils/constants/text_strings.dart
+++ lib/utils/constants/text_strings.dart
@@ -19,4 +19,50 @@ class TTexts {
   // -- Home
   static const String homeAppbarTitle = "Good day for Gifting";
   static const String homeAppbarSubTitle = "UBN Tech Infra";
+
+  // -- Authentication Form Text
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String username = "Username";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgotPassword = "Forgot Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of Use";
+  static const String verificationCode = "Verification Code";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
+
+// -- Authentication Headings Text
+  static const String loginTitle = "Welcome Back";
+  static const String loginSubTitle =
+      "Discover limitless choices and unmatched convenience.";
+
+  static const String signupTitle = "Let's create your account";
+
+  static const String forgotPasswordTitle = "Forgot Password";
+  static const String forgotPasswordSubTitle =
+      "Don't worry, sometimes people forget too. Enter your email and we will send you a password reset link.";
+
+  static const String changeYourPasswordTitle = "Password Reset Email Sent";
+  static const String changeYourPasswordSubTitle =
+      "Your account security is our priority! We've sent you a secure link to safely change your password and keep your account protected.";
+
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle =
+      "Congratulations! Your account awaits. Verify your email to start shopping and experience a world of unmatched deals and personalized offers.";
+
+  static const String emailNotReceivedMessage =
+      "Didn't get the email? Check your junk/spam or resend it.";
+
+  static const String yourAccountCreatedTitle =
+      "Your account has been successfully created!";
 }
